16cdfde1e32b58d86dfac5c7341682e5bf968260
Heather Peterson <hpeterson462@gmail.com>
2020-08-12 17:08:52 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import './App.css';
 import { fetchBirds } from './birds-api.js'
 
 class App extends React.Component() {
-  return(
-    <div className = "App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  state = {
+    birds: []
+  }
+
+  componentDidMount = async () => {
+    const data = await fetchBirds()
+
+    this.setState({
+      birds: data.body
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h2>Birds:</h2>
+        </header>
+        {
+          this.state.birds.map((bird) => {
+            return { birds.name } : {birds.number_of_eggs} : {birds.flies} : {birds.title}
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
