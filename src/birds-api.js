93485f503bfb8cb5501d5f9b39a8b141fe39eef4
Heather Peterson <hpeterson462@gmail.com>
2020-08-17 16:55:42 -0700

    update api url

--- src/birds-api.js
+++ src/birds-api.js
@@ -1,7 +1,6 @@
 import request from 'superagent';
 
-const URL = 'http://localhost:3001';
-//process.env.REACT_APP_API_URL;
+const URL = process.env.REACT_APP_API_URL;
 
 export function fetchBirds() {
     try {
